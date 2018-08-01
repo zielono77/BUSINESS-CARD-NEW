3653b0f6d3ea45df239c838eb75f41761e98fc9f
unknown <thanuszewski@gmail.com>
2018-08-01 14:01:23 +0200

    Add popup window

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     /*! Fades in page on load */
     $('.main-header').css('display', 'none');
-    $('.main-header').fadeIn(2000);
+    $('.main-header').fadeIn(1500);
 
+});
+
+//Popup window
+
+$(function () {
+    $(".lunch-window").click(function (e) {
+        e.preventDefault();
+
+        $("body").css("overflow", "hidden");
+
+        var idWindow = $(this).attr("data-windowId");
+        $(idWindow).fadeIn("fast");
+
+        //Window close
+        $(idWindow).find(".window-close").click(function () {
+            $(idWindow).fadeOut("fast", function () {
+                $("body").css("overflow", "auto");
+            });
+        });
+    });
 });
